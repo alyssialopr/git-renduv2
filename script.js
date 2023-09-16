661bddb106ca6459e0a6739430e7cca91af108da
Alyssia Lorsold Pradon <alyssia.pradon@gmail.com>
2023-09-16 22:26:14 +0200

    :zap: début css et javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+// faire apparaitre et disparaître la 1ere partie du site
+const aly = document.getElementById("prez1");
+
+const btnaly = document.getElementById("aly");
+
+// faire apparaitre et disparaitre la deuxieme partie du site web
+
+const leo = document.getElementById("prez2");
+
+const btnleo = document.getElementById("leo");
+
