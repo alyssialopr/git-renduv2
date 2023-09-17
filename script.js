12268bf71a056c2626b32fdde2768e6cab3adabe
Alyssia Lorsold Pradon <alyssia.pradon@gmail.com>
2023-09-17 14:44:49 +0200

    last commit before merge

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const aly = document.getElementById("prez1");
 
 const btnaly = document.getElementById("aly");
 
+btnaly.onclick = function () {
+  aly.style.display = "block";
+};
+
 // faire apparaitre et disparaitre la deuxieme partie du site web
 
 const leo = document.getElementById("prez2");
 
 const btnleo = document.getElementById("leo");
+
+btnleo.onclick = function () {
+  leo.style.display = "block";
+};
 
