94b5cc368be2cd5f74b7d4a888add0295a22d627
Léora Chriqui <leora.chriqui@gmail.com>
2023-09-17 15:32:37 +0200

    :rocket: on a fait des betises

---
+++ script.js
@@ -0,0 +1,20 @@
+// faire apparaitre et disparaître la 1ere partie du site
+const aly = document.getElementById("prez1");
+
+const btnaly = document.getElementById("aly");
+
+btnaly.onclick = function () {
+  aly.style.display = "block";
+};
+
+// faire apparaitre et disparaitre la deuxieme partie du site web
+
+const leo = document.getElementById("prez2");
+
+const btnleo = document.getElementById("leo");
+
+btnleo.onclick = function () {
+  leo.style.display = "block";
+  aly.style.display = "hidden";
+};
+
